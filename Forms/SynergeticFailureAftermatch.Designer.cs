b429214edc4a90c352a41513d936c201bbef7bae
Дмитрий Александров <aleksandrovd2017@gmail.com>
2024-07-11 06:16:32 +0300

    Готовность: 70%. Реализованы функции моделирования отказа узлов. Начата реализация главной функции.

--- Forms/SynergeticFailureAftermatch.Designer.cs
+++ Forms/SynergeticFailureAftermatch.Designer.cs
@@ -28,20 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FailureLog = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.FailureLog)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FailureLog);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Журнал отказов";
+            // 
+            // FailureLog
+            // 
+            this.FailureLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.FailureLog.Location = new System.Drawing.Point(6, 21);
+            this.FailureLog.Name = "FailureLog";
+            this.FailureLog.RowHeadersWidth = 51;
+            this.FailureLog.RowTemplate.Height = 24;
+            this.FailureLog.Size = new System.Drawing.Size(284, 399);
+            this.FailureLog.TabIndex = 1;
             // 
             // SynergeticFailureAftermatch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "SynergeticFailureAftermatch";
             this.Text = "Поиск последствий отказа";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.FailureLog)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView FailureLog;
     }
 }
